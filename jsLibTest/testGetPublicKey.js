f210aaf79a65d6945b356dd62a1ab906ae7305c1
yin <yin.shen@thoughtworks.com>
2020-05-29 11:22:06 +0800

    refactor: move non-truffle test files

--- jsLibTest/testGetPublicKey.js
+++ jsLibTest/testGetPublicKey.js
@@ -1,7 +1,7 @@
 const Wallet = require('ethereumjs-wallet');
 const EthUtil = require('ethereumjs-util');
 
-const privateKey = "0x4762e04d10832808a0aebdaa79c12de54afbe006bfffd228b3abcc494fe986f9"
+const privateKey = "0x4e77046ba3f699e744acb4a89c36a3ea1158a1bd90a076d36675f4c883864377"
 const publicKey1 = "0x048773a46bc5a2bb1c5687de4788a7d58df3f27483687c8df81d07350753161e05741eafdcfa7f0e1728afc8d4c383aff5b69f886921d13e2c1be77c694fc3362f";
 const publicKey2 = "0x8773a46bc5a2bb1c5687de4788a7d58df3f27483687c8df81d07350753161e05741eafdcfa7f0e1728afc8d4c383aff5b69f886921d13e2c1be77c694fc3362f";
 const compressedPublicKey = "0x038773a46bc5a2bb1c5687de4788a7d58df3f27483687c8df81d07350753161e05";
