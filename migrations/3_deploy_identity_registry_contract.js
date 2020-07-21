b5bb351a830aad12cdf946b8b6ec14ca1da4b73b
Chaowei Zeng <sli616@163.com>
2020-07-21 15:45:40 +0800

    chore: add deploy script for Identities

--- migrations/3_deploy_identity_registry_contract.js
+++ migrations/3_deploy_identity_registry_contract.js
@@ -1,5 +1,7 @@
 const IdentityRegistry = artifacts.require("./IdentityRegistry.sol");
+const Identities = artifacts.require("Identities.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(IdentityRegistry)
+    deployer.deploy(IdentityRegistry);
+    deployer.deploy(Identities);
 };
